aa9829af3ae2de223727d2e32290962a36d4c759
Tami McManus <107127279+tamimcm416@users.noreply.github.com>
2024-07-22 07:12:59 -0500

    folder rename

---
+++ library_survey/sql/CreateTableEstPop.sql
@@ -0,0 +1,9 @@
+USE pub_lib_survey;
+GO
+
+CREATE TABLE [dbo].[est_pop](
+	[STABR] [char](2),
+	[STNAME] [char](20),
+	[ESTPOP] [int]
+) ON [PRIMARY];
+GO
--- tableau_sql_library/sql/CreateTableEstPop.sql
+++
@@ -1,9 +0,0 @@
-USE pub_lib_survey;
-GO
-
-CREATE TABLE [dbo].[est_pop](
-	[STABR] [char](2),
-	[STNAME] [char](20),
-	[ESTPOP] [int]
-) ON [PRIMARY];
-GO
